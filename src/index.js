99d2a33b9a0304fb594dd235dd599bf39618cc16
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-04-30 14:54:35 -0300

    challenge finished

--- src/index.js
+++ src/index.js
@@ -10,19 +10,71 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const countTodo = user.todos.length
+
+  if ((user.pro === false && countTodo <= 9) || (user.pro === true) ) {
+    return next();
+  } else {
+    return response.status(403).json({ 
+      error: "Max limit reached, switch to pro plan!"
+    });
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Id is not valid!' });
+  }
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo || todo.id !== id) {
+    return response.status(404).json({ error: 'Id not found!' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
